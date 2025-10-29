b6cbecdbd96f2380946346361e33d89ed4f2858d
Rohit-coder201 <chhantyalrohit78@gmail.com>
2025-10-29 15:06:41 +0900

    rohit made a change

--- src/kth_range.py
+++ src/kth_range.py
@@ -4,14 +4,52 @@ class Node:
         self.left = None
         self.right = None
 
+
 def insert(root, key):
-    # TODO: return new root after insert (reject duplicates)
-    pass
+    """Insert key into BST, reject duplicates."""
+    if root is None:
+        return Node(key)
+    if key == root.key:
+        return root
+    elif key < root.key:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
+
 
 def kth_smallest(root, k):
-    # TODO
-    pass
+    """Return k-th smallest key (1-indexed). Raise IndexError if k > total nodes."""
+    stack = []
+    cur = root
+    count = 0
+
+    while cur or stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+
+        cur = stack.pop()
+        count += 1
+        if count == k:
+            return cur.key
+
+        cur = cur.right
+
+    raise IndexError("k is out of bounds for the tree size")
+
 
 def range_sum_bst(root, low, high):
-    # TODO
-    pass
+    """Return sum of node keys within [low, high]."""
+    if root is None:
+        return 0
+
+    total = 0
+    if low <= root.key <= high:
+        total += root.key
+    if root.key > low:
+        total += range_sum_bst(root.left, low, high)
+    if root.key < high:
+        total += range_sum_bst(root.right, low, high)
+
+    return total
